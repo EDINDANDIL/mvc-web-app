ee75f1caa60936105b6b43bdbfa9aae192ac12ae
EDINDANDIL <130506909+EDINDANDIL@users.noreply.github.com>
2025-07-27 19:26:46 +0300

    Update PersonValidator.java

--- src/main/java/org/edindandil/app/util/PersonValidator.java
+++ src/main/java/org/edindandil/app/util/PersonValidator.java
@@ -20,33 +20,18 @@ public class PersonValidator implements Validator {
         this.personDAO = personDAO;
     }
 
-    // В этом методе надо указать, на объектах какого класса можно использовать этот валидатор
-    // Так как сам метод validate принимает класс Object, то по сути туда можно подать все что угодно,
-    // а это недопустимо. В методе supports надо указать поддерживаемые классы
     @Override
     public boolean supports(Class<?> clazz) {
         return Person.class.equals(clazz); // проверяем соответствие подаваемого класса и поддерживаемого
     }
 
-    // Тут непосредственно валидация
     @Override
     public void validate(Object target,  Errors errors) {
         Person person = (Person) target;
         Optional<Person> result = personDAO.show(person.getPersonName());
-
-        /* есть ли человек в бд с таким же email?
-        Если есть, то этот метод его найдет и ответ будет не null
-        я в методе указал Optional, поэтому могу пользоваться isPresent(),
-        что является лучшей проверкой, чем != null
-        */
         if (result.isPresent()) {
                 errors.rejectValue("personName", "", "Имя уже есть в списке");
-            /* Обращаемся к объекту, который собирает ошибки, произвести метод rejectValue.
-            В нем должно быть 3 параметра:
-             1) поле, по которому произошла ошибка
-             2) код ошибки (пока не указываю)
-             3) сообщение ошибки
-            */
+        
         }
     }
 }
